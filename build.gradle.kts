d51287efa7b58187d0640103876a61197cb7501e
Eugene Trigubov <trigubov.eugene@gmail.com>
2024-02-24 15:49:55 +0100

    init

---
+++ build.gradle.kts
@@ -0,0 +1,44 @@
+plugins {
+	java
+	id("org.springframework.boot") version "3.2.3"
+	id("io.spring.dependency-management") version "1.1.4"
+}
+
+group = "com"
+version = "0.0.1-SNAPSHOT"
+
+java {
+	sourceCompatibility = JavaVersion.VERSION_17
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	// Core
+	implementation("org.springframework.boot:spring-boot-starter")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework.boot:spring-boot-starter-actuator")
+	implementation("org.springframework.boot:spring-boot-starter-validation")
+
+	// DB
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("org.springframework.boot:spring-boot-starter-jdbc")
+	implementation("org.postgresql:postgresql")
+
+	// Security
+	implementation("org.springframework.boot:spring-boot-starter-security")
+	implementation("org.hibernate:hibernate-validator:6.1.0.Final")
+
+	// Lombok
+	runtimeOnly("com.h2database:h2")
+	compileOnly("org.projectlombok:lombok:1.18.28")
+	annotationProcessor("org.projectlombok:lombok:1.18.28")
+
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
---
+++ src/main/java/com/finbots/FinbotsApplication.java
@@ -0,0 +1,13 @@
+package com.finbots;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinbotsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinbotsApplication.class, args);
+	}
+
+}
