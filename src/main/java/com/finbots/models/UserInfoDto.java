0e6340266a1a96dbbd00b19dec819b72e8ffedd6
Eugene Trigubov <trigubov.eugene@gmail.com>
2024-05-19 17:41:46 +0200

    FIB-97: add models for user info and error details

---
+++ src/main/java/com/finbots/models/UserInfoDto.java
@@ -0,0 +1,20 @@
+package com.finbots.models;
+
+import jakarta.persistence.Column;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserInfoDto {
+    private String email;
+
+    private String tinkoffToken;
+
+    private String role;
+}
